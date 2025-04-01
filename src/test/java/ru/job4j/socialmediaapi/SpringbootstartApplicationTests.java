f50a0240dedf6695b75f6e394a75a34df9babb4d
KatUser <ekaterina.shirokikh@gmail.com>
2025-04-01 18:03:22 +0700

    Spring Data JPA [#505127] add SpringbootstartApplicationTests.java

---
+++ src/test/java/ru/job4j/socialmediaapi/SpringbootstartApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.job4j.socialmediaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+@SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.properties")
+class SpringbootstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
