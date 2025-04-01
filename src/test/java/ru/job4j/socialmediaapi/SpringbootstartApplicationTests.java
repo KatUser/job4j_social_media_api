584b71177c00349d8469827c652e954171d88ea6
KatUser <ekaterina.shirokikh@gmail.com>
2025-04-01 18:33:14 +0700

    Spring Data JPA [#505127]

--- src/test/java/ru/job4j/socialmediaapi/SpringbootstartApplicationTests.java
+++ src/test/java/ru/job4j/socialmediaapi/SpringbootstartApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+
 @SpringBootTest
 @ActiveProfiles("test")
 @TestPropertySource("classpath:application-test.properties")
