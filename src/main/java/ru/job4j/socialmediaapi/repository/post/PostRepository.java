c4e4eb45a2099358a731a5f264f6f952bab40886
KatUser <ekaterina.shirokikh@gmail.com>
2025-04-05 06:45:02 +0700

    3.5.1. Data Именованные запросы

--- src/main/java/ru/job4j/socialmediaapi/repository/post/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/post/PostRepository.java
@@ -1,19 +1,19 @@
 package ru.job4j.socialmediaapi.repository.post;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.socialmediaapi.model.Post;
 import ru.job4j.socialmediaapi.model.User;
 
-import java.awt.print.Pageable;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
-    List<Post> findAllByUser(User user);
+    List<Post> findByUser(User user);
 
-    List<Post> findAllByCreated(LocalDateTime created);
+    List<Post> findByCreatedBetween(LocalDateTime end, LocalDateTime start);
 
-    Page<Post> findAllOrderByCreatedDesc(Pageable pageable);
+    Page<Post> findByOrderByCreatedDesc(Pageable pageable);
 }
